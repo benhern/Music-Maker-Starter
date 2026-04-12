0abcad4482d921b2cd2c9d08ae3f27f4c46e4047
HankDeTank05 <henryhankholman@gmail.com>
2026-04-12 11:09:45 -0500

    Rohan2

--- piano.js
+++ piano.js
@@ -21,7 +21,24 @@ const pianoSynth = new Tone.PolySynth(Tone.Synth, {
 const gMajor = ['G4', 'A4', 'B4', 'C5', 'D5', 'E5', 'F#5'];
 
 
+//this function creates the key through css/js
 
+function createPianoKey(note){
+const key = document.createElement("div");
+key.className = "Key"
+key.setAttribute("data-note", "test");
+
+return key;
+
+}
+
+
+export function renderKeys({container}){
+container.innerHTML = '';
+const key = createPianoKey("tesr")
+
+container.appendChild(key)
+}
 
 
 
--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 import { renderKeys } from './piano.js';
-import { renderSynth } from './synthpad.js';
-import { renderStrings } from './strings.js';
+// import { renderSynth } from './synthpad.js';
+// import { renderStrings } from './strings.js';
+
+const container = document.getElementById('instrument-container');
+const renderContext = { container };
+
+const instrumentBtns = document.querySelectorAll('.instrument-btn');
+
+
+instrumentBtns.forEach((btn) =>{
+
+    btn.addEventListener("click", (event) =>{
+        const instrument=event.currentTarget.getAttribute("data-instrument")
+
+        switch(instrument){
+            case "Piano": 
+                renderKeys(renderContext);
+        }
+    })
+
+
+
+
+
+
+    
+})
+
 
